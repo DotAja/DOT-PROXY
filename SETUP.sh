892e7e23c279fc7d9f7c7b564ac4a5433f71a75f
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-16 01:28:23 +0700

    created hendi kusnandi

--- SETUP.sh
+++ SETUP.sh
@@ -4,19 +4,42 @@ clear
 
 echo "setup dan install..."
 
-apt update -y > /dev/null 2>&1
-apt install dante-server -y > /dev/null 2>&1
+apt update > /dev/null 2>&1
+apt install dante-server > /dev/null 2>&1
 
 clear
 
 echo "sukses..."
 
-wget -O /tmp/danted.conf raw.githubusercontent.com/DotAja/DOT-PROXY/main/danted.conf
+CONFIG_FILE="/etc/danted.conf"
 
-sudo mv /tmp/danted.conf /etc/danted.conf
+NEW_CONFIG="
+internal: ens3 port = 1080
+external: ens3
 
-systemctl restart danted
-systemctl enable danted
+method: username none
+
+client pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+    log: connect disconnect error
+}
+
+pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+    command: bind connect udpassociate
+    log: connect disconnect error
+}"
+
+if [ -w "$CONFIG_FILE" ]; then
+    # Menulis konfigurasi baru ke file
+    echo "$NEW_CONFIG" > "$CONFIG_FILE"
+    echo "Konfigurasi Dante telah berhasil diperbarui."
+else
+    echo "Tidak dapat menulis ke file konfigurasi: $CONFIG_FILE"
+    echo "Pastikan kamu memiliki izin yang diperlukan atau jalankan skrip ini dengan sudo."
+fi
+
+sudo systemctl restart danted
 
 public_ip=$(curl -s ifconfig.me)
 
