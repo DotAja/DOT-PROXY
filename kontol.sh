0605c9ad1ff1713ee3b9a7310f2c8de525661d1b
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-31 00:37:29 +0700

    Update kontol.sh

--- kontol.sh
+++ kontol.sh
@@ -41,22 +41,18 @@ external: ens5
 internal: ens6 port = 1080
 external: ens6
 
-# Menetapkan metode autentikasi
 method: username
 
-# Daftar pengguna dengan password berbeda untuk setiap proxy
 user.privileged: dot ens3 dot1
 user.privileged: dot ens4 dot2
 user.privileged: dot ens5 dot3
 user.privileged: dot ens6 dot4
 
-# Memberikan akses ke semua klien
 client pass {
     from: 0.0.0.0/0 to: 0.0.0.0/0
     log: connect disconnect error
 }
 
-# Memberikan izin akses ke semua koneksi
 pass {
     from: 0.0.0.0/0 to: 0.0.0.0/0
     protocol: tcp udp
@@ -68,16 +64,17 @@ if [ -w "$CONFIG_FILE" ]; then
     echo "ALAT COLI BERHASIL DI TERAPKAN"
 else
     echo "ALAT COLI GAGAL DI TERAPKAN: $CONFIG_FILE"
-    echo "Pastikan kamu memiliki izin yang diperlukan atau jalankan skrip ini dengan sudo."
+    echo "Pastikan kamu memiliki izin yang diperlukan atau jalankan skrip ini dengan sudo"
 fi
 
 sudo systemctl restart danted
 sudo systemctl enable danted
 
 clear
+
 echo "======================================================"
-echo "SOCKS 1 : $ip1:1080:dot:dot1"
-echo "SOCKS 2 : $ip2:1080:dot:dot2"
-echo "SOCKS 3 : $ip3:1080:dot:dot3"
-echo "SOCKS 4 : $ip4:1080:dot:dot4"
+echo "SOCKS1 : $ip1:1080:dot:dot1 "
+echo "SOCKS2 : $ip2:1080:dot:dot2 "
+echo "SOCKS3 : $ip3:1080:dot:dot3 "
+echo "SOCKS4 : $ip4:1080:dot:dot4 "
 echo "==================CREATED BY DOT AJA=================="
