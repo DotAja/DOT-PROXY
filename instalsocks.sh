7867492165a58139fed58daf992f82d44b23523f
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-06 01:12:24 +0700

    Update instalsocks.sh

--- instalsocks.sh
+++ instalsocks.sh
@@ -1,3 +1,3 @@
-wget -O /usr/dotinstall https://raw.githubusercontent.com/DotAja/DOT-PROXY/main/install.sh
-chmod +x /usr/dotinstall
+wget -O /usr/bin/dotinstall https://raw.githubusercontent.com/DotAja/DOT-PROXY/main/install.sh
+chmod +x /usr/bin/dotinstall
 dotinstall
