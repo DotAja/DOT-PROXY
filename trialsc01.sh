7ba97e1bfec75a369b098aae34ac22d22ce18e76
DotAja <142836390+DotAja@users.noreply.github.com>
2024-07-14 23:55:35 +0700

    Update trialsc01.sh

--- trialsc01.sh
+++ trialsc01.sh
@@ -1,20 +1,58 @@
-NC='\e[0m'
-DEFBOLD='\e[39;1m'
-RB='\e[31;1m'
-GB='\e[32;1m'
-YB='\e[33;1m'
-BB='\e[34;1m'
-MB='\e[35;1m'
-CB='\e[36;1m'
-WB='\e[37;1m'
+#!/bin/bash
 
 clear
-echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "           ${WB}----- [ PROXY SOCKS DOT ] -----${NC}              "
-echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "                   ${RB}SCRIPT EXPIRED${NC}"
-echo -e "               ${MB}PENYEWAAN BISA HUBUNGI${NC}"
-echo -e "             ${GB}https://bit.ly/buy_socks-dot${NC}"
-echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "           ${WB}----- [ PROXY SOCKS DOT ] -----${NC}               "
-echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+read -p "Socks name: " ngaran
+read -p "Socks pass: " sentot
+
+clear
+echo "Sedang instalasi..."
+
+apt update > /dev/null 2>&1
+apt install dante-server > /dev/null 2>&1
+
+JALUR_DANTED="/etc/danted.conf"
+JALUR_SOCKD="/etc/pam.d/sockd"
+
+NEW_DANTED="
+internal: ens3 port = 1080
+external: ens3
+
+method: username
+
+clientmethod: none
+user.privileged: root
+user.notprivileged: nobody
+user.libwrap: nobody
+
+client pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+    log: connect disconnect error
+}
+
+pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+    protocol: tcp udp
+}"
+
+NEW_SOCKD="
+auth       required     pam_unix.so
+account    required     pam_unix.so
+"
+
+echo "$NEW_DANTED" | sudo tee "$JALUR_DANTED" > /dev/null
+
+sudo useradd -m -s /bin/false $ngaran
+echo "$ngaran:$sentot" | sudo chpasswd
+
+echo "$NEW_SOCKD" | sudo tee "$JALUR_SOCKD" > /dev/null
+
+sudo systemctl restart danted > /dev/null
+sudo systemctl enable danted > /dev/null
+
+public_ip=$(curl -s ifconfig.me)
+
+clear
+
+echo "======================PROXY SOCKS====================="
+echo "$public_ip:1080:$ngaran:$sentot"
+echo "==================CREATED BY DOT AJA=================="
